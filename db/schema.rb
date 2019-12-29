bcd8d701b0157d9b2c91df3ddc004687eec5c7f8
Christopher Mitov <chrismitov@gmail.com>
2019-12-29 19:17:40 +0200

    Added the model and started implementings the actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_29_145813) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", limit: 128
+    t.string "genre", limit: 32
+    t.text "description"
+    t.string "isbn", limit: 13
+    t.datetime "publish_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+default_books = [{
+  title: 'A Man Called Ove',
+  genre: 'Novel',
+  description: 'A grumpy yet loveable man finds his solitary world turned on its head when a boisterous young family moves in next door.',
+  isbn: '9781871510560',
+  publish_date: '27-Aug-2013'
+}, {
+  title: 'It',
+  genre: 'Horror',
+  description: 'It’s a small city, a place as hauntingly familiar as your own hometown. Only in Derry the haunting is real...',
+  isbn: '9780670813025',
+  publish_date: '15-Sep-1986'
+}, {
+  title: 'Day of the Dragon',
+  genre: 'Fantasy',
+  description: 'A terrifying upheaval among the highest ranks of the world\'s Wizards sends the maverick Mage, Rhonin, on a perilous journey into the Orc  controlled lands of Khaz Modan. What Rhonin uncovers is a vast, far-reaching conspiracy, darker than anything he ever imagined',
+  isbn: '9780671041526',
+  publish_date: '1-Feb-2001'
+}, {
+  title: 'The Return of the King',
+  genre: 'Fantasy',
+  description: 'THE RETURN OF THE KING, which brings to a close the great epic of war and adventure begun in The Fellowship of the Ring and continued in The Two Towers, is the third and final part of J. R. R. Tolkien’s masterpiece, "The Lord of the Rings."',
+  isbn: '9780547928197',
+  publish_date: '20-Oct-1955'
+}, {
+  title: 'The Hobbit',
+  genre: 'Fantasy',
+  description: 'In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort',
+  isbn: '9780547928227',
+  publish_date: '21-Sep-1937'
+}]
+
+default_books.each do |book|
+  Book.create!(book)
+end
