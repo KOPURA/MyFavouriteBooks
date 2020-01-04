7ea0c540425edb4e92e296c43831dc4ec3cb2e97
Christopher Mitov <chrismitov@gmail.com>
2020-01-04 17:42:37 +0200

    Added CRUD

--- app/controllers/BooksController.rb
+++ app/controllers/BooksController.rb
@@ -1,5 +1,44 @@
 class BooksController < ApplicationController
-    def index
-        @books = Book.all
-    end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create!(params.require(:book).permit(*permitted))
+
+    redirect_to books_path, :notice => "Book '#{@book.title} was successfully created!"
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update_attributes!(params.require(:book).permit(*permitted))
+
+    redirect_to books_path, :notice => "Book '#{@book.title}' was successfully updated!"
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path, :notice => "Book '#{@book.title}' was successfully deleted!"
+  end
+
+  private
+
+  def permitted
+    [ :title, :genre, :isbn, :publish_date, :description ]
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,26 @@
 class Book < ApplicationRecord
+
+  ALL_GENRES = [
+    'Science fiction',
+    'Drama',
+    'Action and Adventure',
+    'Romance',
+    'Mystery',
+    'Horror',
+    'Fantasy',
+    'Novel'
+  ]
+
+  validates :title, presence: true
+  validates :genre, presence: true
+
+  def to_hash
+    {
+      'Book Title' => title,
+      'Genre' => genre,
+      'ISBN Number' => isbn,
+      'Publish Date' => publish_date,
+      'Description' => description
+    }
+  end
 end
